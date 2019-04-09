3164c5c02833d7ebda68dc91d25973eb84aecccf
Rawhani <mohammedrawhani@gmail.com>
2019-04-09 17:24:07 +0300

    Updates

---
+++ precache-manifest.b25634a0ea8549358e06f070ea1fd2e4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cde7d17b804da12dbc8fc19d2321dc2",
+    "url": "/lab/static/media/slide_two.5cde7d17.jpg"
+  },
+  {
+    "revision": "124b374c6f10b6be1d6ac4e2ab79fb4a",
+    "url": "/lab/static/media/slide_three.124b374c.jpg"
+  },
+  {
+    "revision": "492772cc3dc61249bfef361f845f4aae",
+    "url": "/lab/static/media/slide_one.492772cc.jpg"
+  },
+  {
+    "revision": "1eb08028d9205ccab261",
+    "url": "/lab/static/js/runtime~main.f80b80cb.js"
+  },
+  {
+    "revision": "f394dd27e00eeac299db",
+    "url": "/lab/static/js/main.e0d4a64c.chunk.js"
+  },
+  {
+    "revision": "53b1424471e78eae8885",
+    "url": "/lab/static/js/2.9ce6c343.chunk.js"
+  },
+  {
+    "revision": "f394dd27e00eeac299db",
+    "url": "/lab/static/css/main.7fbef35f.chunk.css"
+  },
+  {
+    "revision": "08e0b341e3132ccd377fb6a7e47255e7",
+    "url": "/lab/index.html"
+  }
+];
